a91bc8a47a689ec1e91fefa8ba2ea63ef418cfa0
Kazuki H <kazukih0205@gmail.com>
2026-02-15 11:19:56 +0900

    PropImitationHooks: Spoof Google Photos to Pixel XL

--- core/java/com/android/internal/util/PropImitationHooks.java
+++ core/java/com/android/internal/util/PropImitationHooks.java
@@ -50,6 +50,7 @@ public class PropImitationHooks {
     private static final String PACKAGE_GMS = "com.google.android.gms";
     private static final String PROCESS_GMS_UNSTABLE = PACKAGE_GMS + ".unstable";
     private static final String PACKAGE_GPHOTOS = "com.google.android.apps.photos";
+    private static final String FEATURE_NEXUS_PRELOAD = "com.google.android.apps.photos.NEXUS_PRELOAD";
 
     private static final String PACKAGE_SETUPWIZARD = "com.google.android.setupwizard";
     private static final String PACKAGE_SUBSCRIPTION_RED =
@@ -71,6 +72,16 @@ public class PropImitationHooks {
                     "ID", "ZP11.251212.007",
                     "FINGERPRINT",
                             "google/mustang_beta/mustang:CANARY/ZP11.251212.007/14649019:user/release-keys");
+
+    private static final Map<String, String> sPixelOneProps = Map.of(
+            "PRODUCT", "marlin",
+            "DEVICE", "marlin",
+            "MANUFACTURER", "Google",
+            "BRAND", "google",
+            "MODEL", "Pixel XL",
+            "ID", "QP1A.191005.007.A3",
+            "FINGERPRINT", "google/marlin/marlin:10/QP1A.191005.007.A3/5972272:user/release-keys"
+    );
 
     private static final Map<String, String> sPixelTenProps =
             Map.of(
@@ -127,7 +138,7 @@ public class PropImitationHooks {
     private static volatile String sStockFp;
 
     private static volatile String sProcessName;
-    private static volatile boolean sIsPixelDevice, sIsGms, sIsFinsky, sIsPhotos, sIsTablet;
+    private static volatile boolean sIsGms, sIsFinsky, sIsPhotos, sIsTablet;
 
     public static void setProps(Context context) {
         final String packageName = context.getPackageName();
@@ -149,7 +160,6 @@ public class PropImitationHooks {
         sIsTablet = res.getBoolean(R.bool.config_spoofasTablet);
 
         sProcessName = processName;
-        sIsPixelDevice = Build.MANUFACTURER.equals("Google") && Build.MODEL.contains("Pixel");
         sIsGms = packageName.equals(PACKAGE_GMS) && processName.equals(PROCESS_GMS_UNSTABLE);
         sIsFinsky = packageName.equals(PACKAGE_FINSKY);
         sIsPhotos = packageName.equals(PACKAGE_GPHOTOS);
@@ -197,6 +207,10 @@ public class PropImitationHooks {
                     setPropValue("FINGERPRINT", sStockFp);
                     ;
                 }
+                return;
+            case PACKAGE_GPHOTOS:
+                dlog("Spoofing Pixel XL for Google Photos");
+                setProps(sPixelOneProps);
                 return;
         }
     }
@@ -313,13 +327,15 @@ public class PropImitationHooks {
     }
 
     public static boolean hasSystemFeature(String name, boolean has) {
-        if (sIsPhotos
-                && !sIsPixelDevice
-                && has
-                && (sPixelFeatures.stream().anyMatch(name::contains)
-                        || sTensorFeatures.stream().anyMatch(name::contains))) {
-            dlog("Blocked system feature " + name + " for Google Photos");
-            has = false;
+        if (sIsPhotos) {
+            if (has && (sPixelFeatures.stream().anyMatch(name::contains)
+                    || sTensorFeatures.stream().anyMatch(name::contains))) {
+                dlog("Blocked system feature " + name + " for Google Photos");
+                has = false;
+            } else if (!has && name.equalsIgnoreCase(FEATURE_NEXUS_PRELOAD)) {
+                dlog("Enabled system feature " + name + " for Google Photos");
+                has = true;
+            }
         }
         return has;
     }
